a17ff83fd7f619da17886fa84f2dc9891ad1cf5b
Myungjin Hwang <hmjkor@gmail.com>
2018-11-11 20:57:42 +0900

    인코딩 필터 적용

---
+++ src/main/java/kr/co/indisystem/framework/web/MvcWebApplicationInitConfig.java
@@ -0,0 +1,19 @@
+package kr.co.indisystem.framework.web;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+import javax.servlet.FilterRegistration;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+
+public class MvcWebApplicationInitConfig implements WebApplicationInitializer {
+
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException{
+        FilterRegistration charEncodingFilterReg = servletContext.addFilter("CharacterEncodingFilter", CharacterEncodingFilter.class);
+        charEncodingFilterReg.setInitParameter("encoding", "UTF-8");
+        charEncodingFilterReg.setInitParameter("forceEncoding", "true");
+        charEncodingFilterReg.addMappingForUrlPatterns(null, true, "/*");
+    }
+}
